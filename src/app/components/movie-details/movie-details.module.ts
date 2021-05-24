3e051a543fbedab8fe61061c49cb8ef8be68a21a
gesielrosa <Timao159]>
2021-05-24 08:39:12 -0300

    improve imports

--- src/app/components/movie-details/movie-details.module.ts
+++ src/app/components/movie-details/movie-details.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { MovieDetailsComponent } from './movie-details.component';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
